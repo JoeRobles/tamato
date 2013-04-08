607c676bd038818f68562845820aba92b47b87e9
Joe Robles <joe@colldev.com>
2013-04-08 15:15:40 -0500

    tabled disdplaye

--- includes/json.php
+++ includes/json.php
@@ -1,20 +1,21 @@
 <?php
 if (isset($_GET['table']) && $_GET['table'] != '') {
     require_once 'config.php';
+    $tables = $response = array();
+    
     $sql = "SHOW COLUMNS FROM `" . $_GET['table'] . "`;";
     $result = mysql_query($sql);
-    $tables = array();
     while ($table = mysql_fetch_assoc($result)) {
         $tables[] = $table['Field'];
     }
+    $response['th'] = $tables;
     
     $sql = "SELECT `" . implode('`, `', $tables) . "`
             FROM `" . $_GET['table'] . "`;";
     $result = mysql_query($sql);
-    $response = array();
-    $response['th'] = $tables;
     while ($array = mysql_fetch_assoc($result)) {
-        $response['td'] = $array;
+        $response['td'][] = $array;
     }
+
     echo json_encode($response);
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,30 @@
 $(document).on('ready', function(){
     $.getJSON('includes/json.php?table=employee', function(rows) {
-        console.log(rows.th);
-//        var tableList = '';
-//        for (var row in row) {
-//            tableList += '<span data-prod="' + row.id + '" class="red-micro">' + row.name + '</span><br />\n';
-//        }
-//        $('div#content').append(
-//            $('<table />')
-//                .addClass('table')
-//                .attr({
-//                    cellspacing : 0,
-//                    cellpadding : 0
-//                    })
-//                .append(
-//                )
-//        );
+        var $table = $('<table />')
+            .addClass('table')
+            .attr({
+                id : 'table_content',
+                cellspacing : 0,
+                cellpadding : 0
+                });
+        var $thead = $('<thead />');
+        var $tr = $('<tr />');
+        var $tbody = $('<tbody />');
+        
+        $table.append($thead);
+        $thead.append($tr);
+        $(rows.th).each(function(i, name) {
+            $tr.append($('<th />').text(name));
+        });
+        
+        $table.append($tbody);
+        $(rows.td).each(function(j, content) {
+            var $tb = $('<tr />');
+            $(rows.th).each(function(i, name) {
+                $tb.append($('<td />').text(content[name]));
+            });
+            $tbody.append($tb);
+        });
+        $('div#content').append($table);
     });
 });
