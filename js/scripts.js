c1e562803b463625be740ac49dc59b9effb94952
Joe Robles <joe@colldev.com>
2013-04-08 17:59:05 -0500

    object oriented code

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,38 @@
-$(document).on('ready', function(){
-    var table_name = 'employee';
-    $.getJSON('includes/json.php?table=' + table_name, function(rows) {
-        var $table = $('<table />')
-            .addClass('table')
-            .attr({
-                id : 'table_content',
-                cellspacing : 0,
-                cellpadding : 0
-                });
-        var $thead = $('<thead />');
-        var $tr = $('<tr />');
-        var $tbody = $('<tbody />');
-        
-        $table.append($thead);
-        $thead.append($tr);
-        $(rows.th).each(function(i, name) {
-            $tr.append($('<th />').text(name));
-        });
-        
-        $table.append($tbody);
-        $(rows.td).each(function(j, content) {
-            var $tb = $('<tr />');
-            $(rows.th).each(function(i, name) {
-                $tb.append($('<td />').text(content[name]));
-            });
-            $tbody.append($tb);
-        });
-        $('div#content').append($table);
-    });
+$.ajaxSetup({
+    type: "POST",
+    async: false
 });
+var Tamato = {
+    table_name: 'employee',
+    table_attribs: {
+        id : 'table_content',
+        cellspacing : 0,
+        cellpadding : 0
+    },
+    showTable: function() {
+        $.getJSON('includes/json.php?table=' + Tamato.table_name, function(rows) {
+            var $table = $('<table />')
+                .addClass('table')
+                .attr(Tamato.table_attribs);
+            var $thead = $('<thead />');
+            var $tbody = $('<tbody />');
+            var $tr = $('<tr />');
+
+            $table.append($thead);
+            $thead.append($tr);
+            $(rows.th).each(function(i, name) {
+                $tr.append($('<th />').text(name));
+            });
+
+            $table.append($tbody);
+            $(rows.td).each(function(j, content) {
+                var $tb = $('<tr />');
+                $(rows.th).each(function(i, name) {
+                    $tb.append($('<td />').text(content[name]));
+                });
+                $tbody.append($tb);
+            });
+            $('div#content').append($table);
+        });
+    }
+};
