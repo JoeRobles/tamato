42877a4ac3ea855f5c0adf65113973f17b117048
Joe Robles <joe@colldev.com>
2013-04-15 21:42:05 -0500

    remove table added

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,17 @@ $.ajaxSetup({
 });
 
 var Tamato = {
-    actual_row: '',
-    actual_row_id: '',
-    delete_id: 'delete_row',
-    delete_text: 'Eliminar registro',
-    JSON_file: 'includes/json.php',
-    table_name: 'employee',
+    actual_row:      '',
+    actual_row_id:   '',
+    confirm_message: "¿Esta seguro de eliminar este registro?",
+    delete_id:       'delete_row',
+    delete_text:     'Eliminar registro',
+    JSON_file:       'includes/json.php',
+    table_name:      'employee',
     table_attribs: {
         cellspacing: 0,
         cellpadding: 0,
-        class: 'table',
+        class:       'table'
     },
     thead: '',
             
@@ -24,19 +25,19 @@ var Tamato = {
                 if (k !== 0) {
                     var $app = $('<input />')
                         .attr({
-                            type: 'text',
-                            name: Tamato.thead[k],
+                            type:  'text',
+                            name:  Tamato.thead[k],
                             value: $text
-                        })
+                        });
                 } else {
                     var $app = $('<a />')
                         .attr({
-                            alt: Tamato.delete_text,
+                            alt:   Tamato.delete_text,
                             class: Tamato.delete_class,
-                            href: Tamato.JSON_file + '?delete=' + $text,
-                            id: Tamato.delete_id,
+                            href:  Tamato.JSON_file + '?delete=' + $text,
+                            id:    Tamato.delete_id,
                             title: Tamato.delete_question
-                        }).text(Tamato.delete_text)
+                        }).text(Tamato.delete_text);
                 }
                 $(row).text('').append($app);
             }
@@ -49,12 +50,12 @@ var Tamato = {
         $('table.table tr td').on('click', 'a#' + Tamato.delete_id, function(e) {
             e.preventDefault();
             var $this = $(this);
-            var $confirm = confirm("¿Esta seguro de eliminar este registro?");
+            var $confirm = confirm(Tamato.comfirm_message);
             if ($confirm === true) {
                 $.ajax({
-                    url: $this.attr('href'),
+                    url:      $this.attr('href'),
                     dataType: 'json',
-                    data: { to_process: JSON.stringify({ table: Tamato.table_name }) }
+                    data:     { to_process: JSON.stringify({ table: Tamato.table_name }) }
                 }).done(function(){
                     $this.parent().parent().remove();
                 });
@@ -78,6 +79,9 @@ var Tamato = {
             $row.text($new_text);
         });
     },
+    removeTable: function() {
+        $('div#content table').remove();
+    },
     selectRow: function() {
         $('table.table tr').on('click', 'td', function(){
             if (Tamato.actual_row_id !== $(this).parent().attr('id')) {
@@ -85,7 +89,7 @@ var Tamato = {
                     Tamato.updateRow();
                     Tamato.removeInput();
                 }
-                Tamato.actual_row = $(this).parent();
+                Tamato.actual_row    = $(this).parent();
                 Tamato.appendInput();
                 Tamato.actual_row_id = $(this).parent().attr('id');
             }
@@ -126,19 +130,19 @@ var Tamato = {
         $($('table.table tr#' + Tamato.actual_row_id).children()).each(function(k, row) {
             var o = {
                 field_name: $('input', row).attr('name'),
-                value: $('input', row).val()
+                value:      $('input', row).val()
             };
             row_values.push(o);
         });
         var row_content = {
-            table: Tamato.table_name,
+            table:  Tamato.table_name,
             values: row_values
         };
         $.ajax({
-            url: Tamato.JSON_file + '?id=' + Tamato.actual_row_id,
+            url:      Tamato.JSON_file + '?id=' + Tamato.actual_row_id,
             dataType: 'json',
-            data: { to_process: JSON.stringify(row_content) }
-        }).done(function(){
+            data:     { to_process: JSON.stringify(row_content) }
+        }).done(function() {
             console.log('Update done!');
         });
     }
