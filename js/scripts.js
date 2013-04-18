b4eb4a855b4ac9d2743a04f24fae527a2d3a2c89
Joe Robles <joe@colldev.com>
2013-04-17 23:49:02 -0500

    added submit and cacel actions

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,12 @@ $.ajaxSetup({
 var Tamato = {
     actual_row:      '',
     actual_row_id:   '',
+    cancel_action: false,
     confirm_message: "¿Esta seguro de eliminar este registro?",
     delete_id:       'delete_row',
     delete_text:     'Eliminar registro',
     JSON_file:       'includes/json.php',
+    row_content:     '',
     table_name:      'employee',
     table_attribs: {
         cellspacing: 0,
@@ -18,18 +20,13 @@ var Tamato = {
     },
     thead: '',
             
-    appendInput: function() {
-        $($(Tamato.actual_row).children()).each(function(k, row) {
+    appendInput: function(){
+        var row_content = new Array();
+        $($(Tamato.actual_row).children()).each(function(k, row){
             var $text = $(row).text();
+            row_content.push($text);
             if ($text !== '') {
-                if (k !== 0) {
-                    var $app = $('<input />')
-                        .attr({
-                            type:  'text',
-                            name:  Tamato.thead[k],
-                            value: $text
-                        });
-                } else {
+                if (k === 0) {
                     var $app = $('<a />')
                         .attr({
                             alt:   Tamato.delete_text,
@@ -38,16 +35,29 @@ var Tamato = {
                             id:    Tamato.delete_id,
                             title: Tamato.delete_question
                         }).text(Tamato.delete_text);
+                } else {
+                    var $app = $('<input />')
+                        .attr({
+                            type:  'text',
+                            name:  Tamato.thead[k],
+                            value: $text
+                        });
                 }
                 $(row).text('').append($app);
             }
         });
+        Tamato.row_content = row_content;
     },
-    createRow: function() {
+    cancel: function(){
+        Tamato.cancel_action = true;
+        Tamato.removeInput();
+        Tamato.reset();
+    },
+    createRow: function(){
         
     },
-    deleteRow: function() {
-        $('table.table tr td').on('click', 'a#' + Tamato.delete_id, function(e) {
+    deleteRow: function(){
+        $('table.table tr td').on('click', 'a#' + Tamato.delete_id, function(e){
             e.preventDefault();
             var $this = $(this);
             var $confirm = confirm(Tamato.confirm_message);
@@ -62,27 +72,36 @@ var Tamato = {
             } 
         });
     },
-    getTables: function() {
+    getTables: function(){
 
     },
-    removeInput: function() {
-        $($(Tamato.actual_row).children()).each(function(k, row) {
+    removeInput: function(){
+        $($(Tamato.actual_row).children()).each(function(k, row){
             var $row = $(row);
             var $children = $row.children();
-            if (k !== 0) {
-                var $new_text = $children.val();
-            } else {
+            if (k === 0) {
                 var $value = $children.attr('href');
                 var $new_text = $value.split('=').slice(-1)[0];
+            } else {
+                var $new_text = $children.val();
             }
             $children.remove();
+            if (Tamato.cancel_action) {
+                $new_text = Tamato.row_content[k];
+            }
             $row.text($new_text);
         });
     },
-    removeTable: function() {
+    removeTable: function(){
         $('div#content table').remove();
     },
-    selectRow: function() {
+    reset: function(){
+        Tamato.actual_row = '';
+        Tamato.actual_row_id = '';
+        Tamato.row_content = '';
+        Tamato.cancel_action = false;
+    },
+    selectRow: function(){
         $('table.table tr').on('click', 'td', function(){
             if (Tamato.actual_row_id !== $(this).parent().attr('id')) {
                 if (Tamato.actual_row_id !== '') {
@@ -99,8 +118,8 @@ var Tamato = {
     setTable: function (name) {
         
     },
-    showTable: function() {
-        $.getJSON(Tamato.JSON_file + '?table=' + Tamato.table_name, function(rows) {
+    showTable: function(){
+        $.getJSON(Tamato.JSON_file + '?table=' + Tamato.table_name, function(rows){
             var $table = $('<table />').attr(Tamato.table_attribs);
             var $thead = $('<thead />');
             var $tbody = $('<tbody />');
@@ -109,14 +128,14 @@ var Tamato = {
             $table.append($thead);
             $thead.append($tr);
             Tamato.thead = rows.th;
-            $(rows.th).each(function(i, name) {
+            $(rows.th).each(function(i, name){
                 $tr.append($('<th />').text(name));
             });
 
             $table.append($tbody);
-            $(rows.td).each(function(j, content) {
+            $(rows.td).each(function(j, content){
                 var $tb = $('<tr />').attr({ id: content[rows.th[0]]});
-                $(rows.th).each(function(i, name) {
+                $(rows.th).each(function(i, name){
                     $tb.append($('<td />').text(content[name]));
                 });
                 $tbody.append($tb);
@@ -125,9 +144,14 @@ var Tamato = {
         });
         Tamato.selectRow();
     },
-    updateRow: function() {
+    submit: function(){
+        Tamato.updateRow();
+        Tamato.removeInput();
+        Tamato.reset();
+    },
+    updateRow: function(){
         var row_values = new Array();
-        $($('table.table tr#' + Tamato.actual_row_id).children()).each(function(k, row) {
+        $($('table.table tr#' + Tamato.actual_row_id).children()).each(function(k, row){
             var o = {
                 field_name: $('input', row).attr('name'),
                 value:      $('input', row).val()
@@ -150,4 +174,14 @@ var Tamato = {
 
 $(document).on('ready', function(){
     Tamato.showTable();
+});
+$(document).on('keydown', function(event){
+    if(event.which === 27){
+        event.preventDefault();
+        Tamato.cancel();
+    }
+    if(event.which === 13){
+        event.preventDefault();
+        Tamato.submit();
+    }
 });
